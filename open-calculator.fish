e1bdba11159263c18d250b4fdb66473513adfd3d
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-08-11 06:13:18 +1000

    feat: use new API in calculator script

--- open-calculator.fish
+++ open-calculator.fish
@@ -1,24 +1,23 @@
 #!/usr/bin/env fish
 
-source ~/Documents/open-source/fish/gui-menus/utils/message.fish
+source ~/Documents/open-source/fish/gui-menus/utils/inputs.fish
+source ~/Documents/open-source/fish/gui-menus/utils/outputs.fish
 
-set expression (gum input \
-    --header="$(color '❓ Math expression in ' $default_color)$(color 'Fish language' $identifier_color)$(color ': ' $default_color)" \
-    --prompt="🔢 " \
-    --placeholder="Expression...")
+set expression (question_with_input "Math expression in Fish language:" "e.g. 2 + 4")
 
-if test $status -eq 0
-    set temp (mktemp)
-    set result (math "$expression" 2> $temp)
-
-    test $status -ne 0 && begin
-        message "The math expression is incorrect: $(sed --quiet 1p $temp |
-            string replace --regex '^math: Error: ' '')"
-        exit
-    end
-
-    echo $result | fish_clipboard_copy
-    message "$expression = $result (copied to the clipboard)"
-else
+test $status -ne 0 && begin
     message "The math expression calculation has been cancelled."
+    exit
 end
+
+set temp (mktemp)
+set result (math "$expression" 2> $temp)
+
+test $status -ne 0 && begin
+    message "The math expression is incorrect: $(sed --quiet 1p $temp |
+            string replace --regex '^math: Error: ' '')"
+    exit
+end
+
+echo -n $result | fish_clipboard_copy
+message "$expression = $result (copied to the clipboard)"
