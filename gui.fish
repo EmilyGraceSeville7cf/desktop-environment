cc6d08197022f0523d180e10a2386500c6f14896
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-08-12 09:30:28 +1000

    feat: add startup banner

--- gui.fish
+++ gui.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 
 set menu_path $argv[1]
+set arguments $argv[2..]
 set menu_size 70x7
-gnome-terminal --hide-menubar --zoom 1.5 --geometry $menu_size -- $menu_path
+gnome-terminal --hide-menubar --zoom 1.5 --geometry $menu_size $arguments -- $menu_path
---
+++ startup-banner.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+source ~/Documents/open-source/fish/gui-menus/utils/inputs.fish
+
+figlet --gay --width=(tput cols) Love you, Lana!
+pause
